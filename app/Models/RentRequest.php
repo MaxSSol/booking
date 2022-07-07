10951d0bc4a87f0e6bd168649be095cda944baad
Maksym Solomianyi <maxssolom@gmail.com>
2022-07-07 12:58:30 +0300

    Add models.

---
+++ app/Models/RentRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RentRequest extends Model
+{
+    use HasFactory;
+
+    public function accommodation(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Accommodation::class);
+    }
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function paymentMethod(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(PaymentMethod::class);
+    }
+}
