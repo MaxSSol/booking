764c70e201de9ad8c3c5f9ac370413e6ac79d7ed
Maksym Solomianyi <maxssolom@gmail.com>
2022-07-08 11:04:50 +0300

    Add the root App component

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <header-bar/>
+    <router-view/>
+</template>
+
+<script>
+import HeaderBar from "./components/Header/HeaderBar";
+export default {
+    name: "App",
+    components: {HeaderBar}
+}
+</script>
+
+<style scoped>
+
+</style>
