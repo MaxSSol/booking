733deffb8029fddfef3d6b79714c3389f96cb71b
Maksym Solomianyi <maxssolom@gmail.com>
2022-07-17 14:36:46 +0300

    Add the opportunity to add accommodation unit

--- resources/js/components/Footer/FooterBar.vue
+++ resources/js/components/Footer/FooterBar.vue
@@ -2,29 +2,18 @@
     <footer class="mt-8 border-t-2 px-2">
         <div class="container mx-auto grid gap-5 grid-cols-1 lg:grid-cols-6">
             <div class="flex flex-col">
-                <a href="#" class="mt-2 hover:text-blue-700">
+                <router-link to="/" class="mt-2 hover:text-blue-700">
                     Головна
-                </a>
-                <a href="#" class="mt-4 hover:text-blue-700">
+                </router-link>
+                <router-link to="/" class="mt-4 hover:text-blue-700">
                     Обрати помешкання
-                </a>
+                </router-link>
                 <a href="#" class="mt-4 hover:text-blue-700">
                     Про нас
                 </a>
             </div>
-            <div class="flex flex-col">
-                <a href="#" class="mt-2 hover:text-blue-700">
-                    Приватні помешкання
-                </a>
-                <a href="#" class="mt-4 hover:text-blue-700">
-                    Апартаменти
-                </a>
-                <a href="#" class="mt-4 hover:text-blue-700">
-                    Вілли
-                </a>
-            </div>
-            <div></div>
         </div>
+        <div class="flex justify-center">Booking.com.ua 2022</div>
     </footer>
 </template>
 
