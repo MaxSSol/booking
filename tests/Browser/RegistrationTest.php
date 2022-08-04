0c4e2e7e035b4eeae9e47f4913b63c379df27dbb
Maksym Solomianyi <maxssolom@gmail.com>
2022-08-04 11:53:09 +0300

    Add the browser tests

---
+++ tests/Browser/RegistrationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Browser;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class RegistrationTest extends DuskTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function test_registration_page()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/registration')
+                ->type('#email', 'test@email.com')
+                ->type('#first_name', 'Maks')
+                ->type('#password', 'Pa$$w0rd!')
+                ->type('#password_confirmation', 'Pa$$w0rd!')
+                ->press('Зареєструватися')
+                ->pause(1000)
+                ->assertSee('Maks');
+        });
+    }
+}
