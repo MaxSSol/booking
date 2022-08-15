d8b5b2e70951c8328e75a3bc390812afbeb62a28
Maksym Solomianyi <maxssolom@gmail.com>
2022-08-15 14:03:01 +0300

    Add booking details view

--- resources/js/components/RentHistory/RentHistoryItem.vue
+++ resources/js/components/RentHistory/RentHistoryItem.vue
@@ -6,7 +6,8 @@
         <div class="rent-history-room flex flex-col
              md:flex-row justify-center md:justify-between
              mt-6 p-4 border">
-            <accommodation-images :src="'/storage/accommodation/units/'" :images="rentHistory.accommodation_unit?.accommodation_unit_images"
+            <accommodation-images :src="'/storage/accommodation/units/'"
+                                  :images="rentHistory.accommodation_unit?.accommodation_unit_images"
                                   class="h-[200px] w-full lg:w-[400px]"
             />
             <div class="w-2/3 rent-history-room-info ml-0 md:ml-4">
@@ -22,11 +23,15 @@
                     <p class="text-xl font-bold">Ціна за добу: {{ rentHistory.price }}</p>
                     <p class="text-xl font-bold">Разом: {{ rentHistory.total_price }}</p>
                 </div>
-                <a
-                   class="text-base text-white px-8 py-2 bg-blue-900 hover:opacity-50"
+                <popup v-if="popupTrigger" :toggle-popup="popupToggle" :title="'Приховати'">
+                    <rent-history-popup :rent-history="rentHistory"/>
+                </popup>
+                <button @click="popupToggle"
+                        :disabled="rentHistory.check_status === 'unchecked'"
+                        class="text-base text-white px-8 py-2 bg-blue-900 hover:opacity-50"
                 >
                     Переглянути подробиці
-                </a>
+                </button>
             </div>
         </div>
     </div>
@@ -34,14 +39,28 @@
 
 <script>
 import AccommodationImages from "../Accommodation/AccommodationImages";
+import {ref} from "vue";
+import Popup from "../Popup/Popup";
+import RentHistoryPopup from "./RentHistoryPopup";
 
 export default {
     name: "RentHistoryItem",
-    components: {AccommodationImages},
+    components: {RentHistoryPopup, Popup, AccommodationImages},
     props: {
         rentHistory: {
             type: Object,
             require: true
+        }
+    },
+    setup() {
+        let popupTrigger = ref(false)
+        const popupToggle = () => {
+            popupTrigger.value = popupTrigger.value !== true
+        }
+
+        return {
+            popupToggle,
+            popupTrigger
         }
     }
 }
